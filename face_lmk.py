44366e3b60ba04e57fc9c1016916196a8311a1da
xiebian <xiebianwhu@gmail.com>
2022-01-17 13:34:42 +0800

    convert rotation matrix to euler angles

--- face_lmk.py
+++ face_lmk.py
@@ -1,6 +1,15 @@
 import cv2
 import paddlehub as hub
 import numpy as np
+import math
+
+def caculate_euler_angle(rotation_vector, translation_vector):
+    rvec_matrix = cv2.Rodrigues(rotation_vector)[0]
+    proj_matrix = np.hstack((rvec_matrix, translation_vector))
+    euler_angles = cv2.decomposeProjectionMatrix(proj_matrix)[6]
+    # pitch, yaw, roll = [math.radians(_) for _ in euler_angles]
+    pitch, yaw, roll = euler_angles.flatten()
+    return pitch, yaw, roll
 
 def draw_pose(im,image_points):
     size = im.shape
@@ -39,6 +48,7 @@ def draw_pose(im,image_points):
     dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
     (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
 
+    pitch, yaw, roll = caculate_euler_angle(rotation_vector,translation_vector)
     # Project a 3D point (0, 0, 1000.0) onto the image plane.
     # We use this to draw a line sticking out of the nose
 
@@ -58,6 +68,17 @@ def draw_pose(im,image_points):
     p1 = ( int(image_points[0][0]), int(image_points[0][1]))
     p2 = ( int(nose_end_point2D[2][0][0]), int(nose_end_point2D[2][0][1]))
     cv2.line(im, p1, p2, (0,0,255), 2)
+
+    if pitch < 0:
+        pitch+=180
+    else:
+        pitch-=180
+    cv2.putText(im, "pitch: " + "{:7.2f}".format(pitch), (20, int(size[0]/2 -10)), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, (0, 0, 255), thickness=2)
+    cv2.putText(im, "yaw: " + "{:7.2f}".format(yaw), (20, int(size[0]/2 + 30) ), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, (0, 0, 255), thickness=2)
+    cv2.putText(im, "roll: " + "{:7.2f}".format(roll), (20, int(size[0]/2 +70)), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, (0, 0, 255), thickness=2)
 
 model = hub.Module(name='face_landmark_localization')
 
